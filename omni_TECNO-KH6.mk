f492b92263288501c66f7636ece8ef753893a635
rahelaakter2233-star <rahelaakter2233@gmail.com>
2026-07-17 11:37:51 +0600

    Create omni_TECNO-KH6.mk

---
+++ omni_TECNO-KH6.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, device/tecno/TECNO-KH6/device.mk)
+
+PRODUCT_DEVICE := TECNO-KH6
+PRODUCT_NAME := omni_TECNO-KH6
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO KH6
+PRODUCT_MANUFACTURER := TECNO
